f1f94452e69b1732920059b8d8bc73e8fac8b0bf
B0ston <32821943+B0ston@users.noreply.github.com>
2018-06-04 01:11:26 -0700

    added leaderboard, did some other stuff

--- ERBattle/gamemode/cl_init.lua
+++ ERBattle/gamemode/cl_init.lua
@@ -1,10 +1,25 @@
 include("shared.lua")
 
+leaderboard = {}
 -- Initialize font for UI
 surface.CreateFont( "VOL_FONT", {
 	font = "Arial",
 	extended = false,
 	size = 15,
+	weight = 2000,
+})
+
+surface.CreateFont( "LB_FONT", {
+	font = "Consolas",
+	extended = false,
+	size = 15,
+	weight = 2000,
+})
+
+surface.CreateFont( "dmgfont", {
+	font = "Consolas",
+	extended = false,
+	size = 30,
 	weight = 2000,
 })
 
@@ -17,7 +32,12 @@ currentTarget = nil;
 --think hook
 function updateStuff()
 	local ply = LocalPlayer()
-	-- Does not allow false values to linger after client has stopped speaking.
+	local players = player.GetAll()
+	
+	for k, v in pairs(players) do
+		leaderboard[k] = v:VoiceVolume()
+	end
+
 	if !ply:IsSpeaking() then
 		volPercentage = 0
 		vol = 0
@@ -33,9 +53,11 @@ function updateStuff()
 	-- Client networking sending target ent and damage value to server.
 	net.Start("sendVolValue")
 	net.WriteEntity(currentTarget)
-	net.WriteFloat(vol)
+	if (volPercentage > 10) then
+		net.WriteFloat(vol)
+	end
 	net.SendToServer()
-	-- print(currentTarget:Health())
+	
 end
 hook.Add("Think", "continousVolChanger", updateStuff)
 
@@ -46,13 +68,15 @@ hide = {
 }
 
 hook.Add( "HUDShouldDraw", "HideHUD", function( name )
-	if ( hide[ name ] ) then return false end
+	if (hide[name]) then return false end
 end)
 
 hook.Add("HUDPaint", "VolumeUI", function()
 	local opacityR = 50 --red opacity
 	local opacityY = 50 --yellow opacity
 	local opacityG = 50 --green opacity
+	local damage = vol * 75
+	local players = player.GetAll()
 
 	--draw three intensity bars (red yellow green)
 	draw.RoundedBox(0,ScrW() / 2 + 50,ScrH() - 50,100,15,Color(255, 0, 0, opacityR))
@@ -63,19 +87,31 @@ hook.Add("HUDPaint", "VolumeUI", function()
 
 	--Draw text as read if value is above %100.
 	if (volPercentage > 100) then
-		volPercentageColor = Color(255, 0, 0, 255)
+		volPercentageColor = Color(255, 0, 0, 255) --red solid
 	else
-		volPercentageColor = Color(0, 0, 0, 255)
+		volPercentageColor = Color(0, 0, 0, 255) --black solid
 	end
 
 	-- Draw percentage above marker
 	draw.SimpleText( "%"..volPercentage, "VOL_FONT", math.Clamp((ScrW() / 2 - 150) + (300 * (volPercentage / 100)) - 7, ScrW() / 2 - 150, ScrW() / 2 + 150), ScrH() - 70,volPercentageColor)
-
+	draw.SimpleText("Damage: "..math.floor(damage),"dmgfont",50,ScrH() - 40,Color(0, 0, 0, 255))
 	-- Draw outlining rectangle to give sharp look to volume scale
 	surface.SetDrawColor( Color( 0, 0, 0, 255 ) )
 	surface.DrawOutlinedRect( ScrW() / 2 - 150, ScrH() - 50, 300, 15)
 
 	--Draw marker on scale
 	surface.DrawRect( math.Clamp((ScrW() / 2 - 150) + (300 * (volPercentage / 100)), ScrW() / 2 - 150, ScrW() / 2 + 150), ScrH() - 50, 1, 15 )
-end)
 
+	--Draw leaderboard
+	surface.DrawOutlinedRect(20,20,300,200)
+	draw.RoundedBox(0,20,20,300,200,Color(255, 120, 120, 200))
+	draw.SimpleText("Leaderboard:","LB_FONT",20,2,Color(255, 255, 255, 255))
+	local greatest = 0
+	for k, v in pairs(leaderboard) do
+		if v > greatest then
+			draw.SimpleText("~"..players[k]:Nick().." : "..((v / 0.32)),"LB_FONT",40,15 + k * 15,Color(255, 223, 0, 255))
+		else
+			draw.SimpleText(players[k]:Nick().." : "..((v / 0.32)),"LB_FONT",40,15 + k * 15,Color(255, 255, 255, 255))
+		end
+	end
+end)
